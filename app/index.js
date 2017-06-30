54d71ba045d3f6a28afc61aa7ce5f0896638ae79
sourav15 <souravkodali@gmail.com>
2017-06-30 09:16:51 +0530

    github api call

--- app/index.js
+++ app/index.js
@@ -1,11 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Popular = require('./components/popular');
 require('./index.css');
 
 class App extends React.Component {
   render() {
     return (
-      <div> Hello World!</div>
+      <Popular/>
     ) 
   }
 }
---
+++ app/utils/api.js
@@ -0,0 +1,13 @@
+var axios = require('axios');
+
+module.exports = {
+
+  fetchPopularRepos: function(language) {
+    var encodeURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
+
+    return axios.get(encodeURI)
+      .then(function(response) {
+        return response.data.items;
+      });
+  } 
+};
