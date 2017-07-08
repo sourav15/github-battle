a8dee0ecc6c5d03d42437e4d5558c1869967d1db
sourav15 <souravkodali@gmail.com>
2017-07-08 18:22:42 +0530

    battle route

--- app/index.js
+++ app/index.js
@@ -1,15 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Popular = require('./components/popular');
+var App = require('./components/app');
 require('./index.css');
-
-class App extends React.Component {
-  render() {
-    return (
-      <Popular/>
-    ) 
-  }
-}
 
 ReactDOM.render(
   <App />,
